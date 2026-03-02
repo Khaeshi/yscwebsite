95c6e4cdd3213ea273e31576ceadc82097272e5c
Khaeshi <kagtabss@gmail.com>
2026-03-02 21:13:42 +0800

    claud

---
+++ src/lib/db/models/index.ts
@@ -0,0 +1,4 @@
+import Student from './Student.ts';
+import Schedule from './Schedule.ts';
+
+export { Student, Schedule };
--- src/pages/api/schedules/index.ts
+++ src/pages/api/schedules/index.ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from 'astro';
 import { connectDB } from '../../../lib/db/client.ts';
-import Schedule from '../../../lib/db/models/Schedule.ts';
+import { Schedule } from '../../../lib/db/models/index.ts';
 
 const json = (data: any, status = 200) =>
   new Response(JSON.stringify(data), {
