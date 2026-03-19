69d1e454e63907ad17aab9555627abe8fffbb3b7
Khaesey <kagtabss@gmail.com>
2026-03-20 01:06:28 +0800

    re build modules

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,7 @@ export default defineConfig({
     plugins: [tailwindcss()],
     ssr: isProd ? {
       noExternal: true,
+      external: ['@node-rs/argon2', 'oslo']
     } : {},
   },
   site: 'https://youngstarterclub.asia',
--- dist/client/_astro/astro_scripts/before-hydration.js.BJWxGJyl.js
+++
@@ -1,1 +0,0 @@
-import{r}from"../index.D4OJkLir.js";import"../chunk-5RWUYJKV.iI5DGQDX.js";await r({sdkMetadata:{version:"2.17.2",name:"@clerk/astro",environment:"production"}});
